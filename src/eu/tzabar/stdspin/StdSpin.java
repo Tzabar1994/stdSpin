a441833da4f8fd02036d49e51965904eaac97d0a
Daniel Tzabar <daniel@tzabar.org>
2014-11-10 16:56:18 +0000

    First Commit

---
+++ src/eu/tzabar/stdspin/StdSpin.java
@@ -0,0 +1,13 @@
+package eu.tzabar.stdspin;
+
+/**
+ * Created by Dan on 10/11/2014.
+ * It is part of ${PROJECT_NAME}.
+ */
+public class StdSpin
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Test, this is");
+    }
+}
